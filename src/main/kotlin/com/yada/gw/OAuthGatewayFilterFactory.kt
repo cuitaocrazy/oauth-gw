bb03a579ed514d12f92bf216325369e95153642d
lirenhao <renhao.li@bjyada.com>
2020-12-25 17:09:22 +0800

    oauth添加静态资源过滤

--- src/main/kotlin/com/yada/gw/OAuthGatewayFilterFactory.kt
+++ src/main/kotlin/com/yada/gw/OAuthGatewayFilterFactory.kt
@@ -1,6 +1,7 @@
 package com.yada.gw
 
 import org.springframework.beans.factory.ObjectProvider
+import org.springframework.boot.autoconfigure.security.reactive.PathRequest
 import org.springframework.cloud.gateway.filter.GatewayFilter
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory
 import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest
@@ -14,33 +15,37 @@ import reactor.kotlin.core.publisher.switchIfEmpty
 
 @Component
 class OAuthGatewayFilterFactory(private val clientManagerProvider: ObjectProvider<ReactiveOAuth2AuthorizedClientManager>) :
-    AbstractGatewayFilterFactory<OAuthGatewayFilterFactory.Config>(Config::class.java) {
+        AbstractGatewayFilterFactory<OAuthGatewayFilterFactory.Config>(Config::class.java) {
     class Config
 
     override fun apply(config: Config?) = GatewayFilter { exchange, chain ->
-        exchange.getPrincipal<OAuth2AuthenticationToken>()
-            .flatMap { authentication -> authorizedClient(exchange, authentication) }
-            .switchIfEmpty {
-                Mono.error(org.springframework.security.access.AccessDeniedException("Access Denied!"))
-            }.flatMap {
-                chain.filter(exchange)
-            }
+        PathRequest.toStaticResources().atCommonLocations().matches(exchange)
+                .flatMap { chain.filter(exchange) }
+                .switchIfEmpty {
+                    exchange.getPrincipal<OAuth2AuthenticationToken>()
+                            .flatMap { authentication -> authorizedClient(exchange, authentication) }
+                            .switchIfEmpty {
+                                Mono.error(org.springframework.security.access.AccessDeniedException("Access Denied!"))
+                            }.flatMap {
+                                chain.filter(exchange)
+                            }
+                }
     }
 
     private fun authorizedClient(
-        exchange: ServerWebExchange,
-        oauth2Authentication: OAuth2AuthenticationToken
+            exchange: ServerWebExchange,
+            oauth2Authentication: OAuth2AuthenticationToken
     ): Mono<OAuth2AuthorizedClient> {
         val clientRegistrationId = oauth2Authentication.authorizedClientRegistrationId
         val request = OAuth2AuthorizeRequest.withClientRegistrationId(clientRegistrationId)
-            .principal(oauth2Authentication).build()
+                .principal(oauth2Authentication).build()
         val clientManager = clientManagerProvider.ifAvailable
-            ?: return Mono.error(
-                IllegalStateException(
-                    "No ReactiveOAuth2AuthorizedClientManager bean was found. Did you include the "
-                            + "org.springframework.boot:spring-boot-starter-oauth2-client dependency?"
+                ?: return Mono.error(
+                        IllegalStateException(
+                                "No ReactiveOAuth2AuthorizedClientManager bean was found. Did you include the "
+                                        + "org.springframework.boot:spring-boot-starter-oauth2-client dependency?"
+                        )
                 )
-            )
         return clientManager.authorize(request)
     }
 }
